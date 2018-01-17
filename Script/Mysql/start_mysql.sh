ad82a802dbd81fce8b2047496591451d262a1f69
huangzhongzhang <hzz1989@vip.qq.com>
2018-01-17 12:20:46 +0800

    Revert "将基础镜像更新为 centos 7"
    This reverts commit 1925e13

--- Script/Mysql/start_mysql.sh
+++ Script/Mysql/start_mysql.sh
@@ -2,12 +2,12 @@
 set -ex;
 
 # 启动容器中的mysql
-systemctl start mariadb;
+/etc/init.d/mysqld start;
 # 配置mysql初始数据库
 mysql --default-character-set=utf8 -e "source DDL.sql;";
 mysql --default-character-set=utf8 -e "grant all privileges on NPC.* to 'npc'@'%' identified by 'QXCTyPzWEa5rBDs2' WITH GRANT OPTION;";
 mysql --default-character-set=utf8 -e "grant all privileges on NPC.* to 'npc'@'localhost' identified by 'QXCTyPzWEa5rBDs2' WITH GRANT OPTION;";
 mysql -u npc -pQXCTyPzWEa5rBDs2 -D NPC --default-character-set=utf8 -e "show tables;"
-systemctl stop mariadb;
+/etc/init.d/mysqld stop;
 
 /usr/bin/mysqld_safe;
