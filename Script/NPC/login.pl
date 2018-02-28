aedacade857ebe463cab367032cf3425a75fa765
huangzhongzhang <hzz1989@vip.qq.com>
2018-02-28 11:43:17 +0800

    更新触发。

--- Script/NPC/login.pl
+++ Script/NPC/login.pl
@@ -87,10 +87,10 @@ $client->load("SmartReply",data=>{
         notice_limit    => 8 ,  #可选，达到该次数提醒对话次数太多，提醒语来自默认或 notice_reply
         warn_limit      => 10,  #可选,达到该次数，会被警告
         ban_limit       => 12,  #可选,达到该次数会被列入黑名单不再进行回复
-        ban_time        => 1200, #可选，拉入黑名单时间，默认1200秒
+        ban_time        => 3600, #可选，拉入黑名单时间，默认1200秒
         period          => 600, #可选，限制周期，单位 秒
         is_need_at      => 1,  #默认是1 是否需要艾特才触发回复 仅针对群消息
-        keyword         => [qw(NPC)], #触发智能回复的关键字
+        keyword         => [qw(NPC npc)], #触发智能回复的关键字
     });
 
 $client->load("ProgramCode");
