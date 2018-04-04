5b6954d0af359e4615903620d3a4dddf6fe054a2
huangzhongzhang <hzz1989@vip.qq.com>
2018-04-04 12:14:53 +0800

    完善学号计数的功能。

--- Script/NPC/count_classmates.sh
+++ Script/NPC/count_classmates.sh
@@ -9,22 +9,41 @@ API_PORT=5011
 group_info=$(curl -s http://${API_ADDR}:${API_PORT}/openqq/get_group_info)
 
 # 统计总共加入了多少个群
-classes_count=$(echo ${group_info}|jq '.[]|{name}'|awk '{print $2}'|tr -d '"'|grep -v "^$"|wc -l)
+classes_count=$(echo "${group_info}"|jq '.[]|{name}'|awk '{print $2}'|tr -d '"'|grep -v "^$"|wc -l)
 
-for i in {0..$[${classes_count}-1]}
+# 获取数据库中已记录的群记录
+classes_in_record=$(mysql -h database -D NPC --user=npc --password="QXCTyPzWEa5rBDs2" --default-character-set=utf8 -s -e "
+    SELECT gname
+    FROM KnowledgeBase
+    WHERE \`key\` = '学生编号';
+" 2> /dev/null)
+
+for (( i=0 ; i<${classes_count} ; i++ ))
 do
     # 获取组名
-    class_name=$(echo ${group_info}|jq ".[${i}]|{name}"|awk '{print $2}'|tr -d '"'|grep -v "^$")
+    class_name=$(echo "${group_info}"|jq ".[${i}]|{name}"|awk '{print $2}'|tr -d '"'|grep -v "^$")
 
     # 获取组成员名称
-    classmates=$(echo ${group_info}|jq ".[${i}]|.member|.[]|{card}"|awk '{print $2}'|tr -d '"'|grep -v "^$")
+    classmates=$(echo "${group_info}"|jq ".[${i}]|.member|.[]|{card}"|awk '{print $2}'|tr -d '"'|grep -v "^$")
 
     # 获取最后一位童鞋的号数和名字
-    last_classmates=$(echo "$classmates"|egrep "[[:digit:]]*组[[:digit:]]*号"|sort|tail -1)
+    last_classmates=$(echo "${classmates}"|egrep "[[:digit:]]*组[[:digit:]]*号"|sort|tail -1)
 
-    # 插入 Knowledge 表并刷新数据。
-    mysql -h database -D NPC --user=npc --password="QXCTyPzWEa5rBDs2" --default-character-set=utf8 -s -e "
-    INSERT REPLACE INTO KnowledgeBase (gname,\`key\`,value) VALUES ('${class_name}','学生编号','当前最后一位同学的编号为：${last_classmates}');
-    COMMIT;
-    " 2> /dev/null
+    if [[ ${last_classmates} != "" ]]; then
+        # 判断是否已经记录
+        if echo "${classes_in_record}"|grep -w ${class_name} &>/dev/null; then
+            # 更新数据
+            mysql -h database -D NPC --user=npc --password="QXCTyPzWEa5rBDs2" --default-character-set=utf8 -s -e "
+                UPDATE KnowledgeBase
+                SET value = '当前最后一位同学的编号为：${last_classmates}' WHERE gname = '${class_name}' AND \`key\` = '学生编号';
+                COMMIT;
+            " 2> /dev/null
+        else
+            # 插入 Knowledge 表。
+            mysql -h database -D NPC --user=npc --password="QXCTyPzWEa5rBDs2" --default-character-set=utf8 -s -e "
+                REPLACE INTO KnowledgeBase (gname,\`key\`,value) VALUES ('${class_name}','学生编号','当前最后一位同学的编号为：${last_classmates}');
+                COMMIT;
+            " 2> /dev/null
+        fi
+    fi
 done
