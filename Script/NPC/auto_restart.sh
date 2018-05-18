927dce5d2c1ce9303f75dd7a15c0fcbbcbdeb66a
huangzhongzhang <hzz1989@vip.qq.com>
2018-05-18 09:26:38 +0800

    给自动重启添加输出

--- Script/NPC/auto_restart.sh
+++ Script/NPC/auto_restart.sh
@@ -6,8 +6,9 @@ if [[ "$(tail -1 /tmp/nohup.out|grep '二维码'|wc -l)" == "1" ]];then
     echo "$process_string"|awk '{print $2}'|xargs kill -9;
     echo "$(date) Mojo-Webqq进程已杀死！"
     sleep 5
+    set -x
     cd /tmp
-    rm -f mojo_webqq_*
     nohup perl $(echo ${process_string#*perl}) &
+    set +x
     echo "$(date) Mojo-Webqq进程已启动！"
 fi
