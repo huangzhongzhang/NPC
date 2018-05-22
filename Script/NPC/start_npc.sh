0dec44473afbb1827d05d0360058e641d39d5678
huangzhongzhang <hzz1989@vip.qq.com>
2018-05-22 17:14:36 +0800

    优化重启判断，限制重启次数。

--- Script/NPC/start_npc.sh
+++ Script/NPC/start_npc.sh
@@ -1,13 +1,46 @@
 #!/bin/bash
 set -x;
 
-# 启动容器中的crontab和mysql
+# 启动容器中的crontab
 /etc/init.d/crond start;
 
-cd /tmp
-rm -f mojo_webqq_* nohup.out
+# 转换密码格式
 QQ_PASSWD_MD5=$(echo -n "$QQ_PASSWD"|md5sum|awk '{print $1}')
-nohup perl /root/login.pl $QQ_ACCOUNT $QQ_PASSWD_MD5 $SMTP_SERVER $SMTP_PORT $SMTP_FROM $MAIL_TO $SMTP_USER $SMTP_PASSWD &
-sleep 10
-/root/viewqr /tmp/mojo_webqq_qrcode_default.png
-tail -f nohup.out
+
+# 初始化重启次数
+restart_count=0
+
+# 设定重启次数
+restart_total=10
+
+while true
+do
+    cd /root
+    process_string=$(ps -ef|grep login.pl|grep -v grep)
+    if [[ -z "$process_string" ]]; then
+        set -x
+        rm -rf /tmp/mojo_webqq_*
+        nohup perl login.pl $QQ_ACCOUNT $QQ_PASSWD_MD5 $SMTP_SERVER $SMTP_PORT $SMTP_FROM $MAIL_TO $SMTP_USER $SMTP_PASSWD &
+        set +x
+        echo "$(date) Mojo-Webqq进程已启动！"
+        sleep 10
+        ./viewqr /tmp/mojo_webqq_qrcode_$QQ_ACCOUNT.png
+    fi
+    sleep 300
+    if [[ $(tail -10 /tmp/nohup.out|grep '二维码'|wc -l) -gt 1 && $restart_count -lt $restart_total ]];then
+        set -x
+        echo "$process_string"|awk '{print $2}'|xargs kill -9;
+        echo "$(date) Mojo-Webqq进程已杀死！"
+        sleep 5
+        nohup perl login.pl $QQ_ACCOUNT $QQ_PASSWD_MD5 $SMTP_SERVER $SMTP_PORT $SMTP_FROM $MAIL_TO $SMTP_USER $SMTP_PASSWD &
+        set +x
+        echo "$(date) Mojo-Webqq进程已启动！"
+        sleep 10
+        ./viewqr /tmp/mojo_webqq_qrcode_$QQ_ACCOUNT.png
+        let restart_count++
+        echo "$(date) 第 $restart_count 次重启完成！"
+        echo -e "\n"
+    else
+        exit 1;
+    fi
+done
